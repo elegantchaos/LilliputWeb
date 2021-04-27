55ddfe21b996f24b4674e9086a00e1ef7f32f906
Sam Deane <sam@elegantchaos.com>
2021-04-27 17:56:53 +0100

    added workflow

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Lilliput.git", from: "1.0.1"),
+        .package(url: "https://github.com/elegantchaos/Lilliput.git", from: "1.0.2"),
 
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.44.1"),

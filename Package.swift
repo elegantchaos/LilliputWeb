03ba6a24d20cbe708fb308711502012b92c25dac
Sam Deane <sam@elegantchaos.com>
2021-05-03 18:34:19 +0100

    Added loadable/fireable

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Lilliput.git", from: "1.0.8"),
+        .package(url: "https://github.com/elegantchaos/Lilliput.git", from: "1.0.9"),
 
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.44.1"),

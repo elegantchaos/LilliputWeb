8aabd34b832694ad7a78d9ce319c5bfb19e98d1d
Sam Deane <sam@elegantchaos.com>
2021-09-20 14:09:28 +0100

    Updated dependencies to build on XC13

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         .package(url: "https://github.com/elegantchaos/Lilliput.git", from: "1.2.1"),
 
         // 💧 Vapor.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.44.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.48.7"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.32.3"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.2.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.1.1"),

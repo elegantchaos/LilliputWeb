e240ba285f481e138f13e7bbd40cdad880977faf
Sam Deane <sam@elegantchaos.com>
2021-10-20 12:11:55 +0100

    Fixed login

--- Sources/LilliputWeb/Controllers/LoginController.swift
+++ Sources/LilliputWeb/Controllers/LoginController.swift
@@ -61,7 +61,7 @@ struct LoginController: RouteCollection {
                 .thenVerifyLogin(login, with: req)
                 .thenRemoveTokens(with: req)
                 .thenGenerateToken(with: req)
-                .thenRedirect(with: req, to: "/")
+                .thenRedirect(with: req, to: .root)
                 .flatMapError { e in
                     return req.render(LoginPage(request: login), error: e)
                 }
